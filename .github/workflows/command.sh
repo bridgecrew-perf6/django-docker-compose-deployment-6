80f16c37b68a0266e1acee55dc8beb8f9ff2aa2b
Jose Soncco <josesoal@gmail.com>
2022-03-21 22:37:14 -0300

    End of tests

--- .github/workflows/command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git config core.sshCommand 'ssh -i /home/ec2-user/.ssh/id_ed25519' && \
-git pull origin main && \
-docker-compose -f docker-compose-deploy.yml build app && \
-docker-compose -f docker-compose-deploy.yml up --no-deps -d app
