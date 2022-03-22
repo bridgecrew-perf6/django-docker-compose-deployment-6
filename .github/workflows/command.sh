5ad4f013c283bf9d56026cf683769c1fda5d589c
Jose Soncco <josesoal@gmail.com>
2022-03-21 22:05:57 -0300

    Test 13

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-ls -la > delme.txt
+git pull origin main > delme.txt
+docker-compose -f docker-compose-deploy.yml build app 
+docker-compose -f docker-compose-deploy.yml up --no-deps -d app
